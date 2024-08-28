a8d5169076df68a36766d3d6156569a6a3c136f6
Win Tun <wintun.hlaing@outlook.com>
2024-08-28 22:34:32 +0630

    update

---
+++ deploy-aws-cdk/infra/temperature_humidity_lambda/api_paths_methods.py
@@ -0,0 +1,11 @@
+#assign api method 
+# getMethod="GET"
+# postMethod="POST"
+# pathMethod="PATCH"
+# deleteMethod="DELETE"
+api_requests = {"get":"GET","post":"POST","put":"PUT","delete":"DELETE"}
+
+#paths
+api_paths={
+    "health":"/health",
+}
--- deploy-aws-cdk/infra/temperature_humidity_lambda/main.py
+++ deploy-aws-cdk/infra/temperature_humidity_lambda/main.py
@@ -1,8 +1,32 @@
 #main heldler
-import json
+import json,logging
+from custom_encoder import CustomEncoder
+from api_paths_methods import api_requests,api_paths
+
+# Python Logging Service
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
 
 def handler(event, context):
-    return {
-        'statusCode': 200,
-        'body': 'Hello from Lambda!'
+    logger.info(event)
+    httpMethod = event['httpMethod']
+    path = event['path']
+    if httpMethod == api_requests['get'] and path == api_paths['health']:
+        response = buildResponse(200,{"message":"API is healthly and working ok."})
+    else:
+        response= buildResponse(404,{"message":"Not Found"})
+    return response
+
+#Response Build 
+def buildResponse(statusCode,body=None):
+    response = {
+        "statusCode":statusCode,
+        "headers":{
+            "Content-Type": "application/json",
+            "Accesss-Control-Allow-Origin": "*"
+        }
     }
+    if body is not None:
+        response["body"]=json.dumps(body,cls=CustomEncoder)
+    return response
